fb933b4368653652e517dc9fa98af0819b8d53d4
yangyao.yy <yangyao.yy@alibaba-inc.com>
2017-07-15 18:41:30 +0800

    add config.py and runner.py file

---
+++ aliquant/config.py
@@ -0,0 +1,7 @@
+# config file
+import client
+
+appId = "24525088"
+appSecret = "3e7af6afb851ba26b168518fae347b71"
+endpoint = "http://test.open5.net"
+app_bucket = "aliquant-test"
---
+++ runner.py
@@ -0,0 +1,32 @@
+import sys
+import os
+import aliquant.client
+import aliquant.config
+
+client = aliquant.client.DefaultClient(aliquant.config.appId, aliquant.config.appSecret, aliquant.config.endpoint, app_bucket = aliquant.config.app_bucket)
+
+params = {
+  'start_date': '2011-01-01',
+  'end_date': '2017-01-01',
+  'init_cash': 1000000,
+  "bar_type":"d"
+}
+
+def main():
+    if len(sys.argv) < 2:
+        print "[ERROR] Not enough input, usage: python runner.py code_file.py" 
+        return
+
+    code_file = sys.argv[1]
+    if not os.path.exists(code_file):
+        print "[ERROR] code_file [%s] does not exsist" % code_file
+    code = open(code_file).read()
+
+    jobId, r = client.execute(code, params)
+    print 'result:'
+    print r
+    client.plot('logs/job' + jobId + '.log') 
+    return jobId, r
+
+if __name__ == "__main__":
+    main()
